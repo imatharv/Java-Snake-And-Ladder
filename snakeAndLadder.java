0eba09253696e4b3b96e1c9244618574f46fcafb
Atharva Joshi <iamatharvaj@google.com>
2021-04-14 17:15:44 +0530

    Initial commit

---
+++ snakeAndLadder.java
@@ -0,0 +1,8 @@
+public class snakeAndLadder
+{
+	public static void main(String[] args)
+	{
+		int position = 0;
+		System.out.println("Welcome to snake & ladder game, we have a single player at position "+position);
+	}
+}
